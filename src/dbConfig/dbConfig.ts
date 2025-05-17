4cbb268fab866d1f79a3926501ccb2fa0a7f6feb
Anirban Kapil <anirbankapil@gmail.com>
2025-05-17 11:54:35 +0530

    setup

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+export async function connectDB () {
+    try {
+      await mongoose.connect(process.env.MONGO_URL!);
+      const connection = mongoose.connection;   
+      
+      connection.on("connected",()=>{
+        console.log("MongoDB connected successfully")
+      });
+
+      connection.on("error",(error)=>{
+        console.log("Error while connecting error---",error)
+      });
+      process.exit();
+    } catch (error) {
+        console.log("Something went wrong")
+        console.log(error)
+    }
+}
